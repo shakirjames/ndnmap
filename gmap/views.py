cff9315ee4c16910028bd5177c1ce0fae73a265f
Shakir James <shakir.james@gmail.com>
2012-02-29 14:52:40 -0600

    added sparkline view

--- gmap/views.py
+++ gmap/views.py
@@ -1,3 +1,35 @@
+#
+# Copyright (c) 2011 Shakir James and Washington University in St. Louis.
+# All rights reserved
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions
+#  are met:
+#    1. Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#    2. Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#    3. The name of the author or Washington University may not be used 
+#       to endorse or promote products derived from this source code 
+#       without specific prior written permission.
+#    4. Conditions of any other entities that contributed to this are also
+#       met. If a copyright notice is present from another entity, it must
+#       be maintained in redistributions of the source code.
+#
+# THIS INTELLECTUAL PROPERTY (WHICH MAY INCLUDE BUT IS NOT LIMITED TO SOFTWARE,
+# FIRMWARE, VHDL, etc) IS PROVIDED BY THE AUTHOR AND WASHINGTON UNIVERSITY 
+# ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED 
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR WASHINGTON UNIVERSITY 
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+# ARISING IN ANY WAY OUT OF THE USE OF THIS INTELLECTUAL PROPERTY, EVEN IF 
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
 from django.conf import settings
 from django.core import serializers
 from django.core.urlresolvers import reverse 
@@ -9,17 +41,6 @@ from django.views.generic import TemplateView
 
 BW_UPDATE_INTERVAL = getattr(settings, 'GMAP_BW_UPDATE_INTERVAL', 5)
 
-
-class MapView(TemplateView):
-    template_name='gmap/map.html'
-    
-    def render_to_response(self, context):
-        context = RequestContext(self.request, {
-            'bw_url': reverse('xhr_bw', args=(0, )).split('0')[0],
-            'bw_update_interval': BW_UPDATE_INTERVAL*1000, # ms
-        })
-        return super(MapView, self).render_to_response(context)
-    
 
 def bw(request, link , time, rx, tx):
     """Add bandwidth reports"""
@@ -35,7 +56,6 @@ def xhr_bw(request, link):
     data = json.dumps({'rx': rx, 'tx': tx})
     return HttpResponse(data, 'application/json')
 
-
 def json(request, file):
     f = '{0}/gmap/json/{1}.json'.format(settings.SITE_ROOT, file)    
     try:
@@ -43,3 +63,37 @@ def json(request, file):
     except IOError:
         raise Http404
     return HttpResponse(data, 'application/json')
+
+def get_traffic_json(link):
+    """Return Traiffic in bits"""
+    from utils import gviz_api
+    description = {'traffic': ('number','Traffic')}
+    data = []
+    for traffic in Bandwidth.objects.filter(link=link):
+        data.append({'traffic': traffic.rx + traffic.tx})
+    # load it into gviz_api.DataTable
+    data_table = gviz_api.DataTable(description)
+    data_table.LoadData(data)
+    # create a JSon string
+    json = data_table.ToJSon()
+    return json
+
+class MapView(TemplateView):
+    template_name='gmap/map.html'
+
+    def render_to_response(self, context):
+        context = RequestContext(self.request, {
+            'bw_url': reverse('xhr_bw', args=(0, )).split('0')[0],
+            'bw_update_interval': BW_UPDATE_INTERVAL*1000, # ms
+        })
+        return super(MapView, self).render_to_response(context)
+
+class SparkLine(TemplateView):
+    template_name='gmap/sparkline.html'
+
+    def render_to_response(self, context):
+        context = RequestContext(self.request, {
+            'json_data': get_traffic_json(self.kwargs['link']),
+        })
+        return super(SparkLine, self).render_to_response(context)
+
